72d3feb48704defe2e734b8f2da23f5fcf8ebe50
Pau <pau.mateu.jordi@gmail.com>
2021-01-28 00:09:17 +0100

    add files

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+
+
+
+// $(document).ready(function() {
+//   $('.flipTimer').flipTimer({
+
+//   // count up or countdown
+//   direction: 'down',
+
+//   // the target <a href="https://www.jqueryscript.net/time-clock/">date</a>
+//   date: 'January 20, 2015 08:30:30',
+
+//   // callback works only with direction = "down"
+//   callback: function() { alert('times up!'); }
+
+//   });
+//   });
+
+// Your jQuery code
